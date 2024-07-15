b305ec862a88c5fad54686a7419c0e4f6af281e7
mohammad reza <mrghkh12@gmail.com>
2024-07-15 17:32:25 +0330

    feat(script.js): handel error empty input

--- scripts/script.js
+++ scripts/script.js
@@ -6,8 +6,13 @@ const QR_ImageElem = $.querySelector('#QR-Image')
 const generatorBtn = $.querySelector('.generatorBtn')
 
 function generateQrCodeImg() {
-    QR_ImageElem.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputBox.value}`
-    imgBox.classList.add('show-Qr-img')
+    if(inputBox.value.length > 0){
+        QR_ImageElem.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputBox.value}`
+        imgBox.classList.add('show-Qr-img')
+    }else{
+        inputBox.classList.add('emptyInputError')
+        inputBox.addEventListener("animationend" , () => inputBox.classList.remove('emptyInputError'))
+    }
 }
 
 generatorBtn.addEventListener('click', generateQrCodeImg)
