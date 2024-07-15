2eaeff6beed7368957fd00a949d87ba2e953ff0f
mohammad reza <mrghkh12@gmail.com>
2024-07-15 17:23:40 +0330

    feat(script.js): create qr code img gereator with api

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,10 @@ const inputBox = $.querySelector('.inputBox')
 const imgBox = $.querySelector('#imgBox')
 const QR_ImageElem = $.querySelector('#QR-Image')
 const generatorBtn = $.querySelector('.generatorBtn')
+
+function generateQrCodeImg() {
+    QR_ImageElem.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputBox.value}`
+    imgBox.classList.add('show-Qr-img')
+}
+
+generatorBtn.addEventListener('click', generateQrCodeImg)
